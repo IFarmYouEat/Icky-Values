f5dbf3bad8b88c08ae3f6a1efea03f170a42175a
Brock Nelson <Bmnelson87@gmail.com>
2025-06-14 11:55:47 -0500

    added json of values and displayed to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import './App.css'
+import "./App.css";
+import data from "./PersonalValues.json";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(data);
+  }, []);
 
   return (
     <>
       <h1>Hello you Absolute Legend!</h1>
+
+      <button class="bg-blue-500"> Hi </button>
+
+      <div>
+        {items.map((item) => (
+          <div key={item.id}>
+            <h2>{item.value}</h2>
+            <p>{item.shadow_behavior}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
