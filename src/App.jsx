41c5efb1b29766a3e0f4c3f2ccf72b8c321960a1
Brock Nelson <Bmnelson87@gmail.com>
2025-06-18 17:23:12 -0500

    setup brackets

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import data from "./PersonalValues.json";
 import React, { useEffect, useState } from "react";
 
 function App() {
-
   const [items, setItems] = useState([]);
+  const [loser1, setLoser1] = useState([]);
+  const [winner1, setWinner1] = useState([]);
 
   useEffect(() => {
     setItems(data);
@@ -14,15 +15,26 @@ function App() {
     <>
       <h1>Hello you Absolute Legend!</h1>
 
-      <button class="bg-blue-500"> Hi </button>
-
-      <div>
-        {items.map((item) => (
-          <div key={item.id}>
-            <h2>{item.value}</h2>
-            <p>{item.shadow_behavior}</p>
-          </div>
-        ))}
+      <button className="bg-blue-500"> Hi </button>
+      <div className="flex justify-center px-20 w-full">
+        <div className="w-1/2">
+          <h4>Loser Bracket</h4>
+          {loser1.map((loser1) => (
+            <div key={loser1.id}>
+              <h2>{loser1.value}</h2>
+              <p>{loser1.shadow_behavior}</p>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h4>Winner Bracket</h4>
+          {winner1.map((winner1) => (
+            <div key={winner1.id}>
+              <h2>{winner1.value}</h2>
+              <p>{winner1.shadow_behavior}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
