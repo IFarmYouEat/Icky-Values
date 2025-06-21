3e964eda5c34e957f8f78c53f5dd123f5f9712df
Brock Nelson <Bmnelson87@gmail.com>
2025-06-21 15:06:43 -0500

    added buttons for 1st round

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,56 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [loser1, setLoser1] = useState([]);
-  const [winner1, setWinner1] = useState([]);
+  const [loser, setLoser] = useState([]);
+  const [winner, setWinner] = useState([]);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     setItems(data);
   }, []);
 
+  const firstRound = () => {};
+
+const handleTop = () => {
+  setIndex(i => i +2)
+
+};
+
+const handleBottom = () => {
+  setIndex(i => i +2)
+};
+
+const currentTop = items[index];
+const currentBottom = items[index + 1];
+
+
   return (
     <>
-      <h1>Hello you Absolute Legend!</h1>
+      <div className="flex justify-center">
+        <h1>Hello you Absolute Legend!</h1>
+      </div>
+      <div className="flex flex-col justify-center">
+        <p className="flex justify-center">Which coworker behavior would frusterate you more</p>
+        <button className="" onClick={handleTop} disabled={!currentTop}>{currentTop ? currentTop.shadow_behavior : "No more items"}</button>
+        <button className="" onClick={handleBottom}>{currentBottom ? currentBottom.shadow_behavior : "No more items"}</button>
+      </div>
 
-      <button className="bg-blue-500"> Hi </button>
       <div className="flex justify-center px-20 w-full">
         <div className="w-1/2">
           <h4>Loser Bracket</h4>
-          {loser1.map((loser1) => (
-            <div key={loser1.id}>
-              <h2>{loser1.value}</h2>
-              <p>{loser1.shadow_behavior}</p>
+          {loser.map((loser) => (
+            <div key={loser.id}>
+              <h2>{loser.value}</h2>
+              <p>{loser.shadow_behavior}</p>
             </div>
           ))}
         </div>
         <div>
           <h4>Winner Bracket</h4>
-          {winner1.map((winner1) => (
-            <div key={winner1.id}>
-              <h2>{winner1.value}</h2>
-              <p>{winner1.shadow_behavior}</p>
+          {winner.map((winner) => (
+            <div key={winner.id}>
+              <h2>{winner.value}</h2>
+              <p>{winner.shadow_behavior}</p>
             </div>
           ))}
         </div>
